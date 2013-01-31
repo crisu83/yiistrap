22083f02f225ca83aa0d0c8dc119eb2d2a555b69
Christoffer Niska <ChristofferNiska@gmail.com>
2013-01-31 14:18:40 +0200

    Add comments to the menu widget

--- widgets/TbNav.php
+++ widgets/TbNav.php
@@ -1,22 +1,55 @@
 <?php
+/**
+ * TbNav class file.
+ * @author Christoffer Niska <ChristofferNiska@gmail.com>
+ * @copyright Copyright &copy; Christoffer Niska 2013-
+ * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
+ */
 
+/**
+ * Bootstrap navigation menu widget.
+ */
 class TbNav extends CWidget
 {
+	/**
+	 * @var string the menu style.
+	 */
 	public $style = TbHtml::NAV_TABS;
+	/**
+	 * @var array list of menu items. Each menu item is specified as an array of name-value pairs.
+	 */
 	public $items = array();
-	public $itemTemplate;
+	/**
+	 * @var boolean whether the labels for menu items should be HTML-encoded. Defaults to true.
+	 */
 	public $encodeLabel = true;
-	public $activeCssClass = 'active';
+	/**
+	 * @var boolean whether to automatically activate items according to whether their route setting
+	 * matches the currently requested route. Defaults to true.
+	 */
 	public $activateItems = true;
+	/**
+	 * @var boolean whether to activate parent menu items when one of the corresponding child menu items is active.
+	 */
 	public $activateParents = false;
+	/**
+	 * @var boolean whether to hide empty menu items.
+	 */
 	public $hideEmptyItems = true;
+	/**
+	 * @var array HTML attributes for the menu's root container tag.
+	 */
 	public $htmlOptions = array();
-	public $submenuHtmlOptions = array();
+
 	// todo: consider supporting these.
+	//public $submenuHtmlOptions = array();
 	//public $linkLabelWrapper;
 	//public $linkLabelWrapperHtmlOptions=array();
-	public $itemCssClass;
+	//public $itemCssClass;
 
+	/**
+	 * Initializes the widget.
+	 */
 	public function init()
 	{
 		$this->htmlOptions['id'] = $this->getId();
@@ -24,12 +57,22 @@ class TbNav extends CWidget
 		$this->items = $this->normalizeItems($this->items, $route, $hasActiveChild);
 	}
 
+	/**
+	 * Runs the widget.
+	 */
 	public function run()
 	{
 		if (count($this->items))
 			echo TbHtml::nav($this->style, $this->items, $this->htmlOptions);
 	}
 
+	/**
+	 * Normalizes the menu items.
+	 * @param array $items the items to be normalized.
+	 * @param string $route the route of the current request.
+	 * @param boolean $active whether there is an active child menu item.
+	 * @return array the normalized menu items.
+	 */
 	protected function normalizeItems($items, $route, &$active)
 	{
 		foreach ($items as $i => $item)
@@ -83,6 +126,12 @@ class TbNav extends CWidget
 		return array_values($items);
 	}
 
+	/**
+	 * Checks whether a menu item is active.
+	 * @param array $item the menu item to be checked.
+	 * @param string $route the route of the current request.
+	 * @return boolean whether the menu item is active.
+	 */
 	protected function isItemActive($item, $route)
 	{
 		if (isset($item['url']) && is_array($item['url']) && !strcasecmp(trim($item['url'][0], '/'), $route))
