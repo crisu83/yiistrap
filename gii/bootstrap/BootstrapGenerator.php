98bdad2b1ca47293e1fc08b72171ea937a3777a6
Christoffer Niska <ChristofferNiska@gmail.com>
2013-06-19 00:55:29 +0300

    Add gii generator

---
+++ gii/bootstrap/BootstrapGenerator.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * BootstrapGenerator class file.
+ * @author Christoffer Niska <ChristofferNiska@gmail.com>
+ * @copyright Copyright &copy; Christoffer Niska 2013-
+ * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @package bootstrap.gii
+ */
+
+Yii::import('gii.generators.crud.CrudGenerator');
+
+class BootstrapGenerator extends CrudGenerator
+{
+	public $codeModel = 'bootstrap.gii.bootstrap.BootstrapCode';
+}
---
+++ gii/bootstrap/templates/default/_search.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * The following variables are available in this template:
+ * - $this: the BootstrapCode object
+ */
+?>
+<?php echo "<?php\n"; ?>
+/* @var $this <?php echo $this->getControllerClass(); ?> */
+/* @var $model <?php echo $this->getModelClass(); ?> */
+/* @var $form CActiveForm */
+?>
+
+<div class="wide form">
+
+<?php echo "<?php \$form=\$this->beginWidget('bootstrap.widgets.TbActiveForm', array(
+	'action'=>Yii::app()->createUrl(\$this->route),
+	'method'=>'get',
+)); ?>\n"; ?>
+
+<?php foreach($this->tableSchema->columns as $column): ?>
+<?php
+	$field=$this->generateInputField($this->modelClass,$column);
+	if(strpos($field,'password')!==false)
+		continue;
+?>
+	<?php echo "<?php echo ".$this->generateActiveRow($this->modelClass,$column)."; ?>\n"; ?>
+
+<?php endforeach; ?>
+	<div class="form-actions">
+		<?php echo "<?php \$this->widget('bootstrap.widgets.TbButton', array(
+			'buttonType'=>'submit',
+			'type'=>'primary',
+			'label'=>'Search',
+		)); ?>\n"; ?>
+	</div>
+
+<?php echo "<?php \$this->endWidget(); ?>\n"; ?>
+
+</div><!-- search-form -->
