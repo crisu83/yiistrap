6454af05bf40f3e0330169be7d4f489846920744
Sam Stenvall <neggelandia@gmail.com>
2013-08-08 09:35:40 +0300

    partially revert 5dcbc3e87d for gii/bootstrap/templates/default (fixes #92)

--- gii/bootstrap/templates/default/update.php
+++ gii/bootstrap/templates/default/update.php
@@ -5,8 +5,8 @@
  */
 ?>
 <?php echo "<?php\n"; ?>
-    /* @var $this <?php echo $this->getControllerClass(); ?> */
-    /* @var $model <?php echo $this->getModelClass(); ?> */
+/* @var $this <?php echo $this->getControllerClass(); ?> */
+/* @var $model <?php echo $this->getModelClass(); ?> */
 <?php echo "?>\n"; ?>
 
 <?php
@@ -20,13 +20,13 @@ echo "\$this->breadcrumbs=array(
 );\n";
 ?>
 
-    $this->menu=array(
-    array('label'=>'List <?php echo $this->modelClass; ?>', 'url'=>array('index')),
-    array('label'=>'Create <?php echo $this->modelClass; ?>', 'url'=>array('create')),
-    array('label'=>'View <?php echo $this->modelClass; ?>', 'url'=>array('view', 'id'=>$model-><?php echo $this->tableSchema->primaryKey; ?>)),
-    array('label'=>'Manage <?php echo $this->modelClass; ?>', 'url'=>array('admin')),
-    );
-    ?>
+$this->menu=array(
+	array('label'=>'List <?php echo $this->modelClass; ?>', 'url'=>array('index')),
+	array('label'=>'Create <?php echo $this->modelClass; ?>', 'url'=>array('create')),
+	array('label'=>'View <?php echo $this->modelClass; ?>', 'url'=>array('view', 'id'=>$model-><?php echo $this->tableSchema->primaryKey; ?>)),
+	array('label'=>'Manage <?php echo $this->modelClass; ?>', 'url'=>array('admin')),
+);
+?>
 
     <h1>Update <?php echo $this->modelClass . " <?php echo \$model->{$this->tableSchema->primaryKey}; ?>"; ?></h1>
 
