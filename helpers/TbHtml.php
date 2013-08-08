crisu83/yiistrap

commit fe2c6df5b9329277b0252ad655f4afb51ead04d6

Change how tests are bootstrapped and add new tests

helpers/TbHtml.php
@@ -896,7 +896,6 @@ class TbHtml extends CHtml // required in order to access the protected methods 
         $containerOptions = TbArray::popValue('containerOptions', $htmlOptions, array());
 
         $labelOptions = TbArray::popValue('labelOptions', $htmlOptions, array());
-        self::addCssClass('radio', $labelOptions);
         if ($inline) {
             self::addCssClass('inline', $labelOptions);
         }
@@ -915,6 +914,7 @@ class TbHtml extends CHtml // required in order to access the protected methods 
                 $items[] = self::radioButton($name, $checked, $htmlOptions);
             } else {
                 $option = self::radioButton($name, $checked, $htmlOptions);
+                self::addCssClass('radio', $labelOptions);
                 $items[] = self::label($option . ' ' . $label, false, $labelOptions);
             }
         }
@@ -960,7 +960,6 @@ class TbHtml extends CHtml // required in order to access the protected methods 
         $checkAllLast = TbArray::popValue('checkAllLast', $htmlOptions);
 
         $labelOptions = TbArray::popValue('labelOptions', $htmlOptions, array());
-        self::addCssClass('checkbox', $labelOptions);
         if ($inline) {
             self::addCssClass('inline', $labelOptions);
         }
@@ -981,6 +980,7 @@ class TbHtml extends CHtml // required in order to access the protected methods 
                 $items[] = self::checkBox($name, $checked, $htmlOptions);
             } else {
                 $option = self::checkBox($name, $checked, $htmlOptions);
+                self::addCssClass('checkbox', $labelOptions);
                 $items[] = self::label($option . ' ' . $label, false, $labelOptions);
             }
         }
@@ -2350,7 +2350,7 @@ EOD;
     public static function navbarForm($action, $method = 'post', $htmlOptions = array())
     {
         self::addCssClass('navbar-form', $htmlOptions);
-        return CHtml::form($action, $method, $htmlOptions);
+        return self::form($action, $method, $htmlOptions);
     }
 
     /**
@@ -2394,7 +2394,9 @@ EOD;
      */
     public static function link($text, $url = '#', $htmlOptions = array())
     {
-        $htmlOptions['href'] = CHtml::normalizeUrl($url);
+        if ($url !== false) {
+            $htmlOptions['href'] = CHtml::normalizeUrl($url);
+        }
         self::clientChange('click', $htmlOptions);
         return self::tag('a', $htmlOptions, $text);
     }
@@ -2592,6 +2594,7 @@ EOD;
      * @param string $label the button label.
      * @param array $htmlOptions additional HTML attributes.
      * @return string the button.
+     * @throws CException if the button type is valid.
      */
     protected static function createButton($type, $label, $htmlOptions)
     {
@@ -2739,7 +2742,6 @@ EOD;
      */
     public static function dropdown($items, $htmlOptions = array())
     {
-        $htmlOptions['role'] = 'menu';
         self::addCssClass('dropdown-menu', $htmlOptions);
         return self::menu($items, $htmlOptions);
     }
@@ -2964,7 +2966,7 @@ EOD;
     public static function stackedPills($items, $htmlOptions = array())
     {
         $htmlOptions['stacked'] = true;
-        return self::tabs($items, $htmlOptions);
+        return self::pills($items, $htmlOptions);
     }
 
     /**
@@ -2975,6 +2977,12 @@ EOD;
      */
     public static function navList($items, $htmlOptions = array())
     {
+        foreach ($items as $i => $itemOptions) {
+            if (!isset($itemOptions['url']) && !isset($itemOptions['items'])) {
+                $label = TbArray::popValue('label', $itemOptions, '');
+                $items[$i] = self::menuHeader($label, $itemOptions);
+            }
+        }
         return self::nav(self::NAV_TYPE_LIST, $items, $htmlOptions);
     }
 
@@ -3007,7 +3015,7 @@ EOD;
     public static function menu(array $items, $htmlOptions = array(), $depth = 0)
     {
         if (!empty($items)) {
-            TbArray::defaultValue('role', 'menu', $htmlOptions);
+            $htmlOptions['role'] = 'menu';
             $output = self::openTag('ul', $htmlOptions);
             foreach ($items as $itemOptions) {
                 if (is_string($itemOptions)) {
@@ -3016,7 +3024,7 @@ EOD;
                     if (isset($itemOptions['visible']) && $itemOptions['visible'] === false) {
                         continue;
                     }
-                    $options = TbArray::popValue('itemOptions', $itemOptions, array());
+                    $options = TbArray::popValue('htmlOptions', $itemOptions, array());
                     if (!empty($options)) {
                         $itemOptions = TbArray::merge($options, $itemOptions);
                     }
@@ -3027,24 +3035,20 @@ EOD;
                     if (TbArray::popValue('disabled', $itemOptions, false)) {
                         self::addCssClass('disabled', $itemOptions);
                     }
-                    if (TbArray::popValue('header', $itemOptions, false)) {
-                        $output .= self::menuHeader($label, $itemOptions);
+                    if (!isset($itemOptions['linkOptions'])) {
+                        $itemOptions['linkOptions'] = array();
+                    }
+                    $icon = TbArray::popValue('icon', $itemOptions);
+                    if (!empty($icon)) {
+                        $label = self::icon($icon) . ' ' . $label;
+                    }
+                    $items = TbArray::popValue('items', $itemOptions, array());
+                    $url = TbArray::popValue('url', $itemOptions, false);
+                    if (empty($items)) {
+                        $itemOptions['linkOptions']['tabindex'] = -1;
+                        $output .= self::menuLink($label, $url, $itemOptions);
                     } else {
-                        if (!isset($itemOptions['linkOptions'])) {
-                            $itemOptions['linkOptions'] = array();
-                        }
-                        $icon = TbArray::popValue('icon', $itemOptions);
-                        if (!empty($icon)) {
-                            $label = self::icon($icon) . ' ' . $label;
-                        }
-                        $items = TbArray::popValue('items', $itemOptions, array());
-                        $url = TbArray::popValue('url', $itemOptions, false);
-                        if (empty($items)) {
-                            $itemOptions['linkOptions']['tabindex'] = -1;
-                            $output .= self::menuLink($label, $url, $itemOptions);
-                        } else {
-                            $output .= self::menuDropdown($label, $url, $items, $itemOptions, $depth);
-                        }
+                        $output .= self::menuDropdown($label, $url, $items, $itemOptions, $depth);
                     }
                 }
             }
@@ -3078,7 +3082,7 @@ EOD;
      * @param integer $depth the current depth.
      * @return string the generated dropdown.
      */
-    public static function menuDropdown($label, $url, $items, $htmlOptions, $depth = 0)
+    protected static function menuDropdown($label, $url, $items, $htmlOptions, $depth = 0)
     {
         self::addCssClass($depth === 0 ? 'dropdown' : 'dropdown-submenu', $htmlOptions);
         $linkOptions = TbArray::popValue('linkOptions', $htmlOptions, array());
@@ -3317,13 +3321,13 @@ EOD;
 
     /**
      * Generates a pagination.
-     * @param array $links the pagination buttons.
+     * @param array $items the pagination buttons.
      * @param array $htmlOptions additional HTML attributes.
      * @return string the generated pagination.
      */
-    public static function pagination(array $links, $htmlOptions = array())
+    public static function pagination(array $items, $htmlOptions = array())
     {
-        if (!empty($links)) {
+        if (!empty($items)) {
             self::addCssClass('pagination', $htmlOptions);
             $size = TbArray::popValue('size', $htmlOptions);
             if (!empty($size)) {
@@ -3336,7 +3340,7 @@ EOD;
             $listOptions = TbArray::popValue('listOptions', $htmlOptions, array());
             $output = self::openTag('div', $htmlOptions);
             $output .= self::openTag('ul', $listOptions);
-            foreach ($links as $itemOptions) {
+            foreach ($items as $itemOptions) {
                 $options = TbArray::popValue('htmlOptions', $itemOptions, array());
                 if (!empty($options)) {
                     $itemOptions = TbArray::merge($options, $itemOptions);
@@ -3360,15 +3364,15 @@ EOD;
      */
     public static function paginationLink($label, $url, $htmlOptions = array())
     {
-        $itemOptions = TbArray::popValue('itemOptions', $htmlOptions, array());
+        $linkOptions = TbArray::popValue('linkOptions', $htmlOptions, array());
         if (TbArray::popValue('active', $htmlOptions, false)) {
-            self::addCssClass('active', $itemOptions);
+            self::addCssClass('active', $htmlOptions);
         }
         if (TbArray::popValue('active', $htmlOptions, false)) {
-            self::addCssClass('disabled', $itemOptions);
+            self::addCssClass('disabled', $htmlOptions);
         }
-        $content = self::link($label, $url, $htmlOptions);
-        return self::tag('li', $itemOptions, $content);
+        $content = self::link($label, $url, $linkOptions);
+        return self::tag('li', $htmlOptions, $content);
     }
 
     /**
@@ -3406,18 +3410,18 @@ EOD;
      */
     public static function pagerLink($label, $url, $htmlOptions = array())
     {
-        $itemOptions = TbArray::popValue('itemOptions', $htmlOptions, array());
+        $linkOptions = TbArray::popValue('linkOptions', $htmlOptions, array());
         if (TbArray::popValue('previous', $htmlOptions, false)) {
-            self::addCssClass('previous', $itemOptions);
+            self::addCssClass('previous', $htmlOptions);
         }
         if (TbArray::popValue('next', $htmlOptions, false)) {
-            self::addCssClass('next', $itemOptions);
+            self::addCssClass('next', $htmlOptions);
         }
         if (TbArray::popValue('disabled', $htmlOptions, false)) {
-            self::addCssClass('disabled', $itemOptions);
+            self::addCssClass('disabled', $htmlOptions);
         }
-        $content = self::link($label, $url, $htmlOptions);
-        return self::tag('li', $itemOptions, $content);
+        $content = self::link($label, $url, $linkOptions);
+        return self::tag('li', $htmlOptions, $content);
     }
 
     // Labels and badges
@@ -4006,7 +4010,6 @@ EOD;
                 if (isset($itemOptions['visible']) && $itemOptions['visible'] === false) {
                     continue;
                 }
-                self::addCssClass('item', $itemOptions);
                 if ($i === 0) { // first item should be active
                     self::addCssClass('active', $itemOptions);
                 }
@@ -4042,13 +4045,14 @@ EOD;
      */
     public static function carouselItem($content, $label, $caption, $htmlOptions = array())
     {
+        self::addCssClass('item', $htmlOptions);
         $overlayOptions = TbArray::popValue('overlayOptions', $htmlOptions, array());
         self::addCssClass('carousel-caption', $overlayOptions);
         $labelOptions = TbArray::popValue('labelOptions', $htmlOptions, array());
         $captionOptions = TbArray::popValue('captionOptions', $htmlOptions, array());
-        $url = TbArray::popValue('url', $htmlOptions);
-        if ($url) {
-            $content = CHtml::link($content, $url);
+        $url = TbArray::popValue('url', $htmlOptions, false);
+        if ($url !== false) {
+            $content = self::link($content, $url);
         }
         $output = self::openTag('div', $htmlOptions);
         $output .= $content;
@@ -4130,7 +4134,14 @@ EOD;
         if (is_array($className)) {
             $className = implode(' ', $className);
         }
-        $htmlOptions['class'] = isset($htmlOptions['class']) ? $htmlOptions['class'] . ' ' . $className : $className;
+        if (isset($htmlOptions['class'])) {
+            // todo: consider throwing an exception if the class exists instead of skipping the class.
+            if (preg_match("/\b{$className}\b/", $htmlOptions['class']) === 0) {
+                $htmlOptions['class'] .= ' ' . $className;
+            }
+        } else {
+            $htmlOptions['class'] = $className;
+        }
     }
 
     /**

.gitignore
@@ -1,28 +1,3 @@
-# cache directories
-Thumbs.db
-*.DS_Store
-*.empty
-
-#phpstorm project files
-.idea
-
-#netbeans project files
-nbproject
-
-#zend studio project files
-.buildpath
-.project
-.settings
-
-# sass cache files
-*.sass-cache
-
-# mac deployment helpers
-switch
-
-# Node modules
-node_modules
-
 # Composer files
 composer.phar
 vendor/*
@@ -32,7 +7,23 @@ codecept.phar
 tests/_log/*
 tests/acceptance/*
 tests/functional/*
-tests/*suite.yml
+tests/acceptance.suite.yml
+tests/functional.suite.yml
+
+# Node modules
+node_modules
 
 # releases
 releases/
+
+# project files
+.idea/*
+nbproject/*
+
+# mac deployment helpers
+switch
+
+# cache directories
+Thumbs.db
+*.DS_Store
+*.empty

.travis.yml
@@ -4,6 +4,7 @@ php:
   - 5.4
 
 before_script:
+  - composer self-upate
   - composer install --prefer-source
 
 script: vendor/bin/codecept run unit

tests/_bootstrap.php
@@ -4,14 +4,9 @@
 // disable Yii error handling logic
 defined('YII_ENABLE_EXCEPTION_HANDLER') or define('YII_ENABLE_EXCEPTION_HANDLER', false);
 defined('YII_ENABLE_ERROR_HANDLER') or define('YII_ENABLE_ERROR_HANDLER', false);
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+$_SERVER['SCRIPT_NAME'] = '/' . __DIR__;
+$_SERVER['SCRIPT_FILENAME'] = __FILE__;
 
 require(__DIR__ . '/../vendor/yiisoft/yii/framework/yii.php');
-
-$config = array(
-    'basePath' => __DIR__ . '/../..',
-    'aliases' => array(
-        'bootstrap' => __DIR__ . '/..',
-    ),
-);
-
-Yii::createWebApplication($config);
+require(__DIR__ . '/unit/TbTestCase.php');

tests/_helpers/CodeHelper.php
@@ -152,6 +152,15 @@ class CodeHelper extends \Codeception\Module
     }
 
     /**
+     * @param \Symfony\Component\DomCrawler\Crawler $node
+     * @param integer $amount
+     */
+    public function seeNodeNumChildren($node, $amount)
+    {
+        $this->assertEquals($amount, count($node->children()));
+    }
+
+    /**
      * @param \DomElement $node
      * @param string $selector
      * @return boolean
@@ -167,15 +176,14 @@ class CodeHelper extends \Codeception\Module
 
     /**
      * @param mixed $content
-     * @param string $selector
+     * @param string $filter
      * @return \Symfony\Component\DomCrawler\Crawler
      */
-    public function createNode($content, $selector = null)
+    public function createNode($content, $filter = null)
     {
-        $crawler = new \Symfony\Component\DomCrawler\Crawler;
-        $crawler->add($content);
-        if ($selector !== null) {
-            $node = $crawler->filter($selector);
+        $crawler = new \Symfony\Component\DomCrawler\Crawler($content);
+        if ($filter !== null) {
+            $node = $crawler->filter($filter);
             $this->assertNotEquals(null, $node);
             return $node;
         }

tests/unit/CodeGuy.php
@@ -460,13 +460,52 @@ class CodeGuy extends \Codeception\AbstractGuy
      * Documentation taken from corresponding module.
      * ----------------------------------------------
      *
+     * @param \Symfony\Component\DomCrawler\Crawler $node
+     * @param integer $amount
+    * Conditional Assertion: Test won't be stopped on fail
+     * @see Codeception\Module\CodeHelper::seeNodeNumChildren()
+     * @return \Codeception\Maybe
+     */
+    public function canSeeNodeNumChildren($node, $amount) {
+        $this->scenario->addStep(new \Codeception\Step\ConditionalAssertion('seeNodeNumChildren', func_get_args()));
+        if ($this->scenario->running()) {
+            $result = $this->scenario->runStep();
+            return new Maybe($result);
+        }
+        return new Maybe();
+    }
+    /**
+     * This method is generated.
+     * Documentation taken from corresponding module.
+     * ----------------------------------------------
+     *
+     * @param \Symfony\Component\DomCrawler\Crawler $node
+     * @param integer $amount
+     * @see Codeception\Module\CodeHelper::seeNodeNumChildren()
+     * @return \Codeception\Maybe
+     */
+    public function seeNodeNumChildren($node, $amount) {
+        $this->scenario->addStep(new \Codeception\Step\Assertion('seeNodeNumChildren', func_get_args()));
+        if ($this->scenario->running()) {
+            $result = $this->scenario->runStep();
+            return new Maybe($result);
+        }
+        return new Maybe();
+    }
+
+ 
+    /**
+     * This method is generated.
+     * Documentation taken from corresponding module.
+     * ----------------------------------------------
+     *
      * @param mixed $content
-     * @param string $selector
+     * @param string $filter
      * @return \Symfony\Component\DomCrawler\Crawler
      * @see Codeception\Module\CodeHelper::createNode()
      * @return \Codeception\Maybe
      */
-    public function createNode($content, $selector = null) {
+    public function createNode($content, $filter = null) {
         $this->scenario->addStep(new \Codeception\Step\Action('createNode', func_get_args()));
         if ($this->scenario->running()) {
             $result = $this->scenario->runStep();

tests/unit/Dummy.php
@@ -14,6 +14,9 @@ class Dummy extends CModel
     public $uneditable = 'Uneditable text';
     public $search = 'Search query';
     public $textarea = 'Textarea text';
+    public $dropdown = '1';
+    public $radioList = '0';
+    public $checkboxList = array('0', '2');
 
     public function attributeNames()
     {

tests/unit/TbArrayTest.php
@@ -1,22 +1,14 @@
 <?php
 use Codeception\Util\Stub;
 
-Yii::import('bootstrap.helpers.TbArray');
+require(__DIR__ . '/../../helpers/TbArray.php');
 
-class TbArrayTest extends \Codeception\TestCase\Test
+class TbArrayTest extends TbTestCase
 {
    /**
     * @var \CodeGuy
     */
     protected $codeGuy;
-
-    protected function _before()
-    {
-    }
-
-    protected function _after()
-    {
-    }
 
     public function testGetValue()
     {

commit 23be771d203e2d05112c760a7fdd8a2e50dc3763

fix rendering of headers in TbNav (broke in fe2c6df5)

helpers/TbHtml.php
@@ -3040,8 +3040,12 @@ EOD;
                     $items = TbArray::popValue('items', $itemOptions, array());
                     $url = TbArray::popValue('url', $itemOptions, false);
                     if (empty($items)) {
-                        $itemOptions['linkOptions']['tabindex'] = -1;
-                        $output .= self::menuLink($label, $url, $itemOptions);
+                        if (!$url) {
+                            $output .= self::menuHeader($label);
+                        } else {
+                            $itemOptions['linkOptions']['tabindex'] = -1;
+                            $output .= self::menuLink($label, $url, $itemOptions);
+                        }
                     } else {
                         $output .= self::menuDropdown($label, $url, $items, $itemOptions, $depth);
                     }