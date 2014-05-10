2923c358eb14fbb4fbd137bf0ba8dc5c1f7fe3a4
Robert Antoine <rjantoine@gmail.com>
2014-05-10 16:16:48 -0400

    Added support for error displaying in TbHtml activeControlGroup

--- helpers/TbHtml.php
+++ helpers/TbHtml.php
@@ -2066,10 +2066,15 @@ EOD;
      * @param string $attribute the attribute.
      * @param array $htmlOptions additional HTML attributes.
      * @param array $data data for multiple select inputs.
+     * @param bool $showErrors overrides help and color with error information when set to true
      * @return string the generated control group.
      */
-    public static function activeControlGroup($type, $model, $attribute, $htmlOptions = array(), $data = array())
+    public static function activeControlGroup($type, $model, $attribute, $htmlOptions = array(), $data = array(), $showErrors = true)
     {
+        if($showErrors && $model->hasErrors($attribute)) {
+            $htmlOptions['error'] = $model->getError($attribute);
+            $htmlOptions['color'] = self::INPUT_COLOR_ERROR;
+        }
         $color = TbArray::popValue('color', $htmlOptions);
         $groupOptions = TbArray::popValue('groupOptions', $htmlOptions, array());
         $controlOptions = TbArray::popValue('controlOptions', $htmlOptions, array());
