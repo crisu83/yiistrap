2575eadf1208b9152738b2b12f2970c11835aad4
marcovtwout <mr@vtwout.net>
2015-03-20 14:33:29 +0100

    Use javascript:void(0) for popover click to prevent jumping to page top.

--- helpers/TbHtml.php
+++ helpers/TbHtml.php
@@ -4643,7 +4643,7 @@ EOD;
         $htmlOptions['rel'] = 'popover';
         $htmlOptions['data-content'] = $content;
         $htmlOptions['data-toggle'] = 'popover';
-        return self::tooltipPopover($label, '#', $title, $htmlOptions);
+        return self::tooltipPopover($label, 'javascript:void(0)', $title, $htmlOptions);
     }
 
     /**
